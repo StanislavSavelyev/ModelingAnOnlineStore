219e50a07bc643b27543f0a54b6e567030fa815b
StanislavSavelyev <texnikum2288@mail.ru>
2025-12-02 15:27:19 +0500

    Создал класс для продукта и создал продукты

--- Modeling an online store/Program.cs
+++ Modeling an online store/Program.cs
@@ -2,8 +2,25 @@
 {
     internal class Program
     {
+        public class Product
+        {
+            string Name { get; set; }
+            decimal Price { get; set; }
+            public Product(string name, decimal price)
+            {
+                Name = name;
+                Price = price;
+            }
+        }
+
         static void Main(string[] args)
         {
+            var prodect1 = new Product("Хлеб", 50);
+            var prodect2 = new Product("Молоко", 100);
+            var prodect3 = new Product("Кетчуп", 40);
+            var prodect4 = new Product("Печеньк", 80);
+            var prodect5 = new Product("Сок", 150);
+            var prodect6 = new Product("Йогурт", 120);
         }
     }
 }
